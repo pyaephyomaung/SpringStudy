1849bbfcfe0928eb8b1bd08f477c0b065394afaa
pyaephyomaung <samuelfruit@gmail.com>
2023-09-29 02:24:40 +0900

    18.CircularDependencyException

---
+++ 18.CircularDependencyException/CircularDependencyException/src/main/java/com/example/bean/Vehicle.java
@@ -0,0 +1,28 @@
+package com.example.bean;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name ;
+
+    @Autowired
+    private Person person;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Vehicle name is "+name;
+    }
+}
