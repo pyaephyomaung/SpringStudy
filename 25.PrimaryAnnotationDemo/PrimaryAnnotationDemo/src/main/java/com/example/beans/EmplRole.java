3f2f06065bf34392d88b0feddce119ffcbfae83e
pyaephyomaung <samuelfruit@gmail.com>
2023-12-23 21:50:54 +0900

    Primary Annotation Demo

---
+++ 25.PrimaryAnnotationDemo/PrimaryAnnotationDemo/src/main/java/com/example/beans/EmplRole.java
@@ -0,0 +1,8 @@
+package com.example.beans;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmplRole implements  RoleInterface{
+}
